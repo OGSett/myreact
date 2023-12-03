62a4840b91f929cea38a4c0bb6b36709144bc651
OGSett <ecomseth@gmail.com>
2023-12-03 19:31:12 +0100

    space project first push

--- space/src/App.js
+++ space/src/App.js
@@ -1,10 +1,55 @@
-import './App.css';
+import { Route, Routes, useLocation, BrowserRouter as Router} from 'react-router-dom';
+import { useEffect } from 'react';
+import Home from "./home"
+import Crew from "./crew"
+import Technology from "./technology"
+// import navbar from "./"
+import Destination from "./destination"
+import useFetch from './useFetch';
+
 
 function App() {
+
+  const { data, isLoading, error } = useFetch();
+    const BodyStyleHandler = () => {
+      const location = useLocation();
+      useEffect(() => {
+        console.log("Current path:", location.pathname);
+        document.body.className = ''; 
+        if (location.pathname === '/home')
+        {
+          document.body.classList.add('home-style');
+        } 
+        else if (location.pathname === '/')
+        {
+          document.body.classList.add('home-style');
+        }
+        else if (location.pathname === '/destination')
+        {
+          document.body.classList.add('destination-style');
+        }
+        else if (location.pathname === '/crew')
+        {
+          document.body.classList.add('crew-style');
+        }
+        else if (location.pathname === '/technology')
+        {
+          document.body.classList.add('technology-style');
+        }
+      }, [location.pathname]);  
+    };
+
   return (
-    <div className="App">
-      <h1>salam</h1>
-    </div>
+    <Router>
+      <BodyStyleHandler />
+      <Routes>
+        <Route path='/' element={<Home data={data} isLoading={isLoading} error={error}/>}/>
+        <Route path='/home' element={<Home data={data} isLoading={isLoading} error={error}/>}/>
+        <Route path='/crew' element={<Crew data={data} isLoading={isLoading} error={error}/>}/>
+        <Route path='/destination' element={<Destination data={data} isLoading={isLoading} error={error}/>}/>
+        <Route path='/technology' element={<Technology data={data} isLoading={isLoading} error={error}/>}/>
+      </Routes>
+    </Router>
   );
 }
 
